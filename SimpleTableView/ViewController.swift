9e2e9811aab2b15fc5a7a88b7bfcce7b74e0091f
Santhosh <clicomdeveloper@gmail.com>
2022-01-17 22:07:51 +0530

    Initial Commit

---
+++ SimpleTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTableView
+//
+//  Created by Santhosh on 17/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
